b8835a5c87c35046304e0c1c0f4044745ced0513
victorpaularony <smallypauls@gmail.com>
2025-03-24 23:17:44 +0300

    Define the URL patterns.

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from django.contrib.auth import views
+from django.contrib.auth.views import LogoutView
 
-from .forms import UserLoginForm
-from . import views as v
+from .views import register_customer, register_company, login_view, home
 
 urlpatterns = [
-    path('', v.register, name='register'),
-    path('company/', v.CompanySignUpView.as_view(), name='register_company'),
-    path('customer/', v.CustomerSignUpView.as_view(), name='register_customer'),
-    path('login/', v.LoginUserView, name='login_user')
+    path('', home, name='home'),
+    path('company/',register_company , name='register_company'),
+    path('customer/', register_customer, name='register_customer'),
+    path('login/', login_view, name='login'),
+     path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
 ]
