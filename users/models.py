62da1627053f80690c94b4249c944a37235eacb2
victorpaularony <smallypauls@gmail.com>
2025-03-24 22:26:56 +0300

    Define two user types: Customer and Company.

--- users/models.py
+++ users/models.py
@@ -1,22 +1,40 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email is required")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def creat_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_admin', True)
+        return self.creat_superuser(email, password, **extra_fields)
 
 class User(AbstractUser):
     is_company = models.BooleanField(default=False)
     is_customer = models.BooleanField(default=False)
     email = models.EmailField(unique=True)
+    username = models.CharField(max_length=100)
+
+    objects = UserManager()
 
     REQUIRED_FIELDS = ["email"]
+    USERNAME_FIELD = ["username"]
+
+    def __str__(self):
+        return self.email
 
 
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     birth = models.DateField(null=True, blank=True)  # Add birth field
 
-    def __str__(self):
-        return f"Customer: {self.user.username}"
 
 
 class Company(models.Model):
